995132b76640ce166022b14db5a3d1139815a277
Loren Snow <lorensnow@protonmail.com>
2025-03-19 09:55:47 -0700

    use STM device headers

--- include/gpio.h
+++ include/gpio.h
@@ -1,37 +1,40 @@
-
+/**
+ ******************************************************************************
+ * @file    gpio.h
+ * @author  Loren Snow
+ * @brief   GPIO header file.
+ *
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 Loren Snow
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ ******************************************************************************
+ */
 
 #ifndef GPIO_H
 #define GPIO_H
 
+#include "stm32f303xe.h"
 #include <stdint.h>
-
-#define GPIOA_BASE (0x48000000UL)
-#define GPIOB_BASE (0x48000400UL)
-#define GPIOC_BASE (0x48000800UL)
-#define GPIOD_BASE (0x48000C00UL)
-#define GPIOE_BASE (0x48001000UL)
-#define GPIOF_BASE (0x48001400UL)
-#define GPIOG_BASE (0x48001800UL)
-#define GPIOH_BASE (0x48001C00UL)
-
-/**
- * @brief   Represents a GPIO port and its registers
- */
-typedef struct
-{
-    volatile uint32_t MODER;   ///< Mode register
-    volatile uint32_t OTYPER;  ///< Output type register
-    volatile uint32_t OSPEEDR; ///< Output speed register
-    volatile uint32_t PUPDR;   ///< Pull-up/pull-down register
-    volatile uint32_t IDR;     ///< Input data register
-    volatile uint32_t ODR;     ///< Output data register
-    volatile uint32_t BSRR;    ///< Bit set/reset register
-    volatile uint32_t LCKR;    ///< Configuration lock register
-    volatile uint32_t AFRL;    ///< Alternate function low register
-    volatile uint32_t AFRH;    ///< Alternate function high register
-    volatile uint32_t BRR;     ///< Bit reset register
-
-} GPIO_TypeDef;
 
 /**
  * @brief   Definitions for GPIO modes
